78b948d5c4f0d475bc47f449156d55fabf4ab0fd
victormainardi <victor.emf_@hotmail.com>
2021-08-21 14:45:08 -0300

    Update projeto_final.ino

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -1,8 +1,8 @@
 //include das libs que vai ser usado
 
-//REFERENCIAS
-//https://microcontrollerslab.com/arduino-freertos-structure-queue-receive-data-multiple-resources/
-//https://www.arduino.cc/en/Reference.AnalogRead
+// REFERENCIAS
+// https://microcontrollerslab.com/arduino-freertos-structure-queue-receive-data-multiple-resources/
+// https://www.arduino.cc/en/Reference.AnalogRead
 
 
 #include <Arduino_FreeRTOS.h>
@@ -34,12 +34,49 @@ void thread_temperatura (void *pvParameters);
 void thread_temperatura_new (void *pvParameters);
 void thread_led (void *pvParameters);
 
-
-
-
 //liga desliga
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  Serial.print("Conectando");
+
+  pinMode(pinLed, OUTPUT);
+  //semaforo
+  if (xSerialSemaphore == NULL){
+    xSerialSemaphore = xSemaphoreCreateMutex();
+    if ((xSerialSemaphore) != NULL){
+      xSemaphoreGive ((xSerialSemaphore));
+    }
+  }
+
+  //criar pilha das temperaturas do sensor
+  structQueue = xQueueCreate(10, sizeof(struct pinRead));
+
+  //testa criação da pilha
+  if(structQueue != NULL){
+  //cria threads com xTaskCreate
+  //https://www.embarcados.com.br/rtos-para-iniciantes-com-arduino-e-freertos/
+    xTaskCreate(thread_analogic, "analogic", 128, NULL, 2, NULL);
+    xTaskCreate(thread_temperatura, "temperatura", 128, NULL, 2, NULL);
+    xTaskCreate(thread_temperatura_new, "temperatura_new", 128, NULL, 2, NULL);
+    xTaskCreate(thread_led, "led", 128, NULL, 2, NULL);
+
+    /*
+    void thread_analogic (void *pvParameters);
+    void thread_temperatura (void *pvParameters);
+    void thread_temperatura_new (void *pvParameters);
+    void thread_led (void *pvParameters);
+    */
+
+
+    
+  }
+  
+  
+
 
 }
 
