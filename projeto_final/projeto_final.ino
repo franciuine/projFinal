63f2a0b2569dae2941baedd2a0390d8872daed7f
victormainardi <victor.emf_@hotmail.com>
2021-08-21 14:33:59 -0300

    Update projeto_final.ino

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -1,4 +1,10 @@
 //include das libs que vai ser usado
+
+//REFERENCIAS
+//https://microcontrollerslab.com/arduino-freertos-structure-queue-receive-data-multiple-resources/
+//https://www.arduino.cc/en/Reference.AnalogRead
+
+
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
@@ -13,7 +19,21 @@ SemaphoreHandle_t xSerialSemaphore;
 struct lerpino{
   int pino;
   float valor;
-}
+};
+
+float bufferTemp[10];
+const int pinLed = 11;
+
+//This lines defines the hanlder for structQueue to access it for tasks reference.
+QueueHandle_t structQueue;
+
+
+//funcoes
+void thread_analogic (void *pvParameters);
+void thread_temperatura (void *pvParameters);
+void thread_temperatura_new (void *pvParameters);
+void thread_led (void *pvParameters);
+
 
 
 
