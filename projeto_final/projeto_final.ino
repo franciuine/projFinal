51541ac53f1515a803740c4f966af27f2ccfa6b9
victormainardi <victor.emf_@hotmail.com>
2021-08-21 00:02:53 -0300

    Update projeto_final.ino

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -1,9 +1,23 @@
+//include das libs que vai ser usado
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
 
+// Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
+// It will be used to ensure only only one Task is accessing this resource at any time.
+SemaphoreHandle_t xSerialSemaphore;
 
+
+//struct ler dados do sensor
+struct input{
+  int pino;
+  float valor;
+}
+
+
+
+//liga desliga
 void setup() {
   // put your setup code here, to run once:
 
