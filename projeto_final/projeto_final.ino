a0449e82a9b11ba2f1d20095d05faefa4f498407
victormainardi <victor.emf_@hotmail.com>
2021-08-25 23:00:48 -0300

    Update projeto_final.ino

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -4,24 +4,14 @@
 // https://microcontrollerslab.com/arduino-freertos-structure-queue-receive-data-multiple-resources/
 // https://www.arduino.cc/en/Reference.AnalogRead
 
-
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
 
-
-
 // Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
 // It will be used to ensure only only one Task is accessing this resource at any time.
 SemaphoreHandle_t xTempSemaphore;
-//struct ler dados do sensor
-struct lerpino{
-  int pino;
-  float valor;
-};
-
-
 
 //********************************************//
 //              DECLARAÇÕES                //
@@ -35,54 +25,40 @@ int ar_cond = 10;
 //Definindo pino do sensor de temperatura no pino A0
 int pino_lm = A0;
 
-//This lines defines the hanlder for structQueue to access it for tasks reference.
-QueueHandle_t structQueue;
-
 //funcoes
 void task_leituraSensor (void *pvParameters);
-void task_temperatura (void *pvParameters);
-void task_mediaTemperatura (void *pvParameters);
 void task_atuadores (void *pvParameters);
+void task_display (void *pvParameters);
 //liga desliga
-void setup() {
-	
-  pinMode(pino_lm, INPUT);
-  pinMode(LED_VM, OUTPUT);
-  pinMode(LED_VD, OUTPUT);
-  pinMode(ar_cond, OUTPUT);
+void setup(){
+	pinMode(pino_lm, INPUT);
+	pinMode(LED_VM, OUTPUT);
+	pinMode(LED_VD, OUTPUT);
+	pinMode(ar_cond, OUTPUT);
   
-  //Define a quantidade de colunas e linhas do LCD
-  LCD.begin(16,2);
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  //checa criação semaforo
-  if (xTempSemaphore == NULL){
-    //criando mutex
-    xTempSemaphore = xSemaphoreCreateMutex();
+	//Define a quantidade de colunas e linhas do LCD
+	LCD.begin(16,2);
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	//checa criação semaforo
+	if (xTempSemaphore == NULL){
+		//criando mutex
+		xTempSemaphore = xSemaphoreCreateMutex();
     if((xTempSemaphore) != NULL){
-      //libera serial
-      xSemaphoreGive((xTempSemaphore));
-    }
-  }
-
-  //criar pilha das temperaturas do sensor LM35
-  structQueue = xQueueCreate(10, sizeof(struct lerpino));
-
-  //testa criação da pilha
-  if(structQueue != NULL){
-	//cria task com xTaskCreate
-	//https://www.embarcados.com.br/rtos-para-iniciantes-com-arduino-e-freertos/
-    //criar task produtora da pilha
-    xTaskCreate(task_leituraSensor, "leituraSensor", 128, NULL, 2, NULL);
-    //criar task produtora da pilha
-    xTaskCreate(task_atuadores, "atuadores", 128, NULL, 2, NULL);
-    //criar task consumidora da pilha
-    xTaskCreate(task_display, "display", 128, NULL, 2, NULL);	
-  }
+		//libera serial
+		xSemaphoreGive((xTempSemaphore));
+		}
+	}
+	//cria task leitura dos sensores
+	xTaskCreate(task_leituraSensor, "leituraSensor", 128, NULL, 2, NULL);
+	//cria task dos atuadores
+	xTaskCreate(task_atuadores, "atuadores", 128, NULL, 2, NULL);
+	//cria task display
+	xTaskCreate(task_display, "display", 128, NULL, 2, NULL);	
 }
 
 void loop(){
-//ja era!!!! FREERTOS N USA LOOP	
+//ja era!!!! FreeRTOS N USA LOOP	
 }
 
 //funcao da task do input analogico, sensor LM35
