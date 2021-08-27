e112ec2567819c456ae580e0ab209309af296b29
victormainardi <victor.emf_@hotmail.com>
2021-08-27 00:28:17 -0300

    Update projeto_final.ino

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -1,26 +1,10 @@
 /**
-//-------------------------------------------------------------------------//
-//                  Universidade Federal de Santa Maria                    //
-//                   Curso de Engenharia de Computação                     //
-//                ELC 1048 - Projeto de Sistemas Embarcados                //
-//                                                                         //
-//   Desenvolvedores:                                                      //
-//       Franciuíne Barbosa da Silva de Almeida (2019520031)               //
-//       Victor Eugenio Mainardi Fritz (201621156)                         //
-//   Professor:                                                            //
-//       Carlos Henrique Barriquello                                       //
-//   Data: 25/08/2020                                                      //
-//=========================================================================//
-//                         Descrição do Programa                           //
-//=========================================================================//
-//  Projeto desenvolvido para avaliação 2 da disciplina.                   //
-//  Controle de ar condicionado através do monitoramento e sensoriamento   //
-//  da temperatura ambiente.                                               //
-//  Utilizando a biblioteca FreeRTOS para Arduino.                         //
-//  Sensor: LM35                                                           //
-//  Saída: LED's e LCD                                                     //
-//-------------------------------------------------------------------------//
+* @file projeto_final.ino
+* @author by Franciuíne Barbosa da Silva de Almeida, Victor Eugenio Mainardi Fritz
+* @link https://github.com/franciuine/projFinal
+* @date 08-2021
 */
+
 
 #include <LiquidCrystal.h>
 #include <Arduino_FreeRTOS.h>
@@ -49,10 +33,7 @@ int LED_VD = 8;
 int ar_cond = 10;
 int pino_lm = A0;
 
-/**
-  DECLARA AS TAREFAS
-*/
-
+/// DECLARACAO DAS TAREFAS
 void task_leituraSensor (void *pvParameters);
 void task_atuadores (void *pvParameters);
 void task_display (void *pvParameters);
@@ -63,23 +44,14 @@ void setup()
   pinMode(LED_VM, OUTPUT);
   pinMode(LED_VD, OUTPUT);
   pinMode(ar_cond, OUTPUT);
-  /**
-    DEFINE A QUANTIDADE DE LINHAS E COLUNAS DO LCD
-  */
+/// DEFINE A QUANTIDADE DE LINHAS E COLUNAS DO LCD
   LCD.begin(16, 2);
   Serial.begin(9600);
-<<<<<<< HEAD
-  //checa criação semaforo
-=======
-  /**
-    VERIFICA SE O SEMÁFORO JÁ EXISTE
-  */
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
+
+/// VERIFICA SE O SEMÁFORO JÁ EXISTE
   if (xTempSemaphore == NULL)
   {
-    /**
-      SE AINDA NÃO EXISTE, CRIA NOVO MUTEX
-    */
+/// SE AINDA NÃO EXISTE, CRIA NOVO MUTEX
     xTempSemaphore = xSemaphoreCreateMutex();
     if ((xTempSemaphore) != NULL)
     {
@@ -87,97 +59,58 @@ void setup()
       xSemaphoreGive(xTempSemaphore);
     }
   }
-  /**
-    CRIA TAREFA PARA PER O SENSOR
-  */
+/// CRIA TAREFA PARA PER O SENSOR
   xTaskCreate(task_leituraSensor, "leituraSensor", 128, NULL, 2, NULL);
-  /**
-    CRIA TAREFA DOS ATUADORES
-  */
+/// CRIA TAREFA DOS ATUADORES
   xTaskCreate(task_atuadores, "atuadores", 128, NULL, 2, NULL);
-  /**
-    CRIA TAREFA DI DISPLAY
-  */
+/// CRIA TAREFA DI DISPLAY
   xTaskCreate(task_display, "display", 128, NULL, 2, NULL);
 }
 
-<<<<<<< HEAD
-//Não usa loop
 void loop()
 {
-	
+/// O CÓDIGO SERÁ EXECUTADO APENAS NAS TAREFAS
 }
 
-//task para leitura do sensor
-=======
-void loop()
-{
-  /**
-    O CÓDIGO SERÁ EXECUTADO APENAS NAS TAREFAS
-  */
-}
-
-/**
-  LEITURA DE DADOS
-*/
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
+/// TASK LEITURA DO SENSOR
 void task_leituraSensor(void *pvParameters)
 {
   while (1)
   {
+/// INPUT ANALOGICO  
     int SensorTempTensao = analogRead(pino_lm);
+/// CODIFICACAO DO SINAL
     float Tensao = SensorTempTensao * 5;
     Tensao /= 1023;
-    /**
-      CONVERTE A TEMPERATURA PARA GRAUS CELSIUS
-    */
+/// CONVERSAO PRA GRAUS CELCIUS
     float Temperatura_atual = (Tensao - 0.5) * 100;
-<<<<<<< HEAD
-	//adquiri o semaforo
+/// TAKE SEMAPHORE
     if (xSemaphoreTake(xTempSemaphore, (TickType_t)5) == pdTRUE)
     {
-      //atualiza temperatura Global
-=======
-    /**
-      ADQUIRI SEMÁFORO E, SE NÃO ESTIVER LANÇADO, BLOQUEIA A THREAD POR 5 TICKS
-    */
+
+/// TENTA ADQUIRIR SEMAFORO
     if (xSemaphoreTake(xTempSemaphore, (TickType_t)5) == pdTRUE)
     {
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
       Temperatura = Temperatura_atual;
       xSemaphoreGive(xTempSemaphore);
     }
-<<<<<<< HEAD
-    //delay da leitura
-=======
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
+/// DELAY NA LEITURA
     vTaskDelay(1);
   }
 }
-
-/**
-  TAREFA PARA CONTROLE DOS LED'S
-*/
+/// TAREFA PARA CONTROLE DOS LED'S
 void task_atuadores(void *pvParameters)
 {
   float temp = 0.0;
   while (1)
   {
-<<<<<<< HEAD
-    //adquiri semaforo
-=======
-    /**
-      ADQUIRI SEMÁFORO E, SE NÃO ESTIVER LANÇADO, BLOQUEIA A THREAD POR 5 TICKS
-    */
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
+/// ADQUIRI SEMAFORO
     if (xSemaphoreTake(xTempSemaphore, (TickType_t)5) == pdTRUE)
     {
       temp = Temperatura;
       xSemaphoreGive(xTempSemaphore);
     }
-    /**
-      CONTROLE DO AR CONDICIONADO (LIGA/DESLICA)
-    */
+/// CONTROLE DO AR CONDICIONADO (LIGA/DESLICA)
     if (temp >= 32.0)
     {
       digitalWrite(ar_cond, HIGH);
@@ -186,10 +119,7 @@ void task_atuadores(void *pvParameters)
     {
       digitalWrite(ar_cond, LOW);
     }
-
-    /**
-      SINAIS DE ALERTA E OK
-    */
+/// SINAIS DE ALERTA
     if (temp > 28.0)
     {
       digitalWrite(LED_VM, HIGH);
@@ -202,29 +132,16 @@ void task_atuadores(void *pvParameters)
     }
   }
 }
-
-/**
-  TAREFA QUE CONTROLA A SAÍDA DE DADOS NO DISPLAY
-*/
+/// TAREFA QUE CONTROLA A SAÍDA DE DADOS NO DISPLAY
 void task_display(void *pvParameters)
 {
   float temp = 0.0;
   while (1)
   {
-<<<<<<< HEAD
-    //adquiri semaforo
+/// ADQUIRI SEMÁFORO
     if (xSemaphoreTake(xTempSemaphore, (TickType_t)5) == pdTRUE)
     {
       temp = Temperatura;
-      //libera semaforo
-=======
-    /**
-      ADQUIRI SEMÁFORO E, SE NÃO ESTIVER LANÇADO, BLOQUEIA A THREAD POR 5 TICKS
-    */
-    if (xSemaphoreTake(xTempSemaphore, (TickType_t)5) == pdTRUE)
-    {
-      temp = Temperatura;
->>>>>>> a706020c69300a740bdd4026940c37ebad13500f
       xSemaphoreGive(xTempSemaphore);
     }
 	
